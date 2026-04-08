4c709bab0d3f85d1f9d9f19ad061ac31b48a7270
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-04-08 22:15:41 +0700

    feat(index.js): add task_3

--- homework_4/index.js
+++ homework_4/index.js
@@ -6,8 +6,15 @@
 // console.log(getSquare(5));
 
 /* 2. Сделайте функцию, которая возвращает сумму двух чисел. */
-function getSum(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
-}
+// function getSum(firstNumber, secondNumber) {
+//   return firstNumber + secondNumber;
+// }
 
-console.log(getSum(30, 44));
+// console.log(getSum(30, 44));
+
+/* 3. Сделайте функцию, которая отнимает от первого числа второе и делит на третье. */
+const subtractAndDivide = function (firstNumber, secondNumber, thirdNumber) {
+  return (firstNumber - secondNumber) / thirdNumber;
+};
+
+console.log(subtractAndDivide(12, 6, 3));
