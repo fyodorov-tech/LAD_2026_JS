dffc220014b6bfc7fd6239ec37ff8b9fdb4d6e9b
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-04-08 22:14:47 +0700

    feat(index.js): add task_2

--- homework_4/index.js
+++ homework_4/index.js
@@ -1,6 +1,13 @@
 /* 1. Сделайте функцию, которая возвращает квадрат числа. Число передается параметром. */
-function getSquare(number) {
-  return number ** 2;
+// function getSquare(number) {
+//   return number ** 2;
+// }
+
+// console.log(getSquare(5));
+
+/* 2. Сделайте функцию, которая возвращает сумму двух чисел. */
+function getSum(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
 
-console.log(getSquare(5));
+console.log(getSum(30, 44));
