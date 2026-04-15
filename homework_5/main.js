ca069f3204e22eb51fce4fc0c941181ed394ea72
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-04-16 04:54:13 +0700

    feat(index.js): add task_3

--- homework_5/main.js
+++ homework_5/main.js
@@ -4,6 +4,11 @@
 // console.log(wordsArray);
 
 /* 2. Напишите функцию deleteСharacters(str, length), которая возвращает подстроку, состоящую из указанного количества символов. */
-const deleteCharacters = (str, length) => str.substr(0, length);
-const str = "You don't know JS";
-console.log(deleteCharacters(str, 5));
+// const deleteCharacters = (str, length) => str.substr(0, length);
+// const str = "You don't know JS";
+// console.log(deleteCharacters(str, 5));
+
+/* 3. Напишите функцию insertDash(str), которая принимает строку str в качестве аргумента и вставляет тире (-) между словами. При этом все символы строки необходимо перевести в верхний регистр. */
+const insertDash = str => str.replaceAll(" ", "-").toUpperCase();
+const str = "Life is too short to skip kebab";
+console.log(insertDash(str));
