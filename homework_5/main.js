74877b001fe58e9d932559fa1e4ba0eee2296531
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-04-16 04:38:22 +0700

    feat(index.js): add task_2

--- homework_5/main.js
+++ homework_5/main.js
@@ -1,4 +1,9 @@
 /* 1. Преобразовать строку в массив слов. Напишите функцию stringToarray(str), которая преобразует строку в массив слов. */
-const stringToArray = str => str.split(" ");
-const wordsArray = stringToArray("Welcome to Lad-academy!!!");
-console.log(wordsArray);
+// const stringToArray = str => str.split(" ");
+// const wordsArray = stringToArray("Welcome to Lad-academy!!!");
+// console.log(wordsArray);
+
+/* 2. Напишите функцию deleteСharacters(str, length), которая возвращает подстроку, состоящую из указанного количества символов. */
+const deleteCharacters = (str, length) => str.substr(0, length);
+const str = "You don't know JS";
+console.log(deleteCharacters(str, 5));
