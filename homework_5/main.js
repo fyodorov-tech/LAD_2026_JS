e71f2702c7299aa6978da324d9de6fd287dcb940
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-04-16 12:01:05 +0700

    feat(main.js): add task_6

--- homework_5/main.js
+++ homework_5/main.js
@@ -19,13 +19,43 @@
 // console.log(makeFirstToUpper(str));
 
 /* 5.Напишите функцию capitalize(str), которая возвращает строку, в которой каждое слово начинается с заглавной буквы. */
-const capitalize = str => {
-  const strArray = str
-    .split(" ")
-    .map(word => `${word[0].toUpperCase()}${word.slice(1)}`);
+// const capitalize = str => {
+//   const strArray = str
+//     .split(" ")
+//     .map(word => `${word[0].toUpperCase()}${word.slice(1)}`);
 
-  return strArray.join(" ");
+//   return strArray.join(" ");
+// };
+
+// const str = "Собран из багов и кофе";
+// console.log(capitalize(str));
+
+/* 6. Напишите функцию changeRegister(str), которая принимает в качестве аргумента строку и и заменяет регистр каждого символа на противоположный. Например, если вводится «КаЖдЫй ОхОтНиК», то на выходе должно быть «кАжДыЙ оХоТнИк». */
+
+/* Вариант с использованием массивов */
+// const changeRegister = str => {
+//   const strArray = str.split("");
+//   changedArray = strArray.map(ch =>
+//     ch === ch.toUpperCase() ? ch.toLowerCase() : ch.toUpperCase()
+//   );
+//   return changedArray.join("");
+// };
+// const str = "КаЖдЫй ОхОтНиК";
+// console.log(changeRegister(str));
+
+/* Вариант без использования массивов */
+const changeRegister = str => {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    result +=
+      str[i] === str[i].toUpperCase()
+        ? str[i].toLowerCase()
+        : str[i].toUpperCase();
+  }
+
+  return result;
 };
 
-const str = "Собран из багов и кофе";
-console.log(capitalize(str));
+const str = "КаЖдЫй ОхОтНиК";
+console.log(changeRegister(str));
