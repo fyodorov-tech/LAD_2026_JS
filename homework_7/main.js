2f6fc3fdba8b5b4ad56bf9789f3d74bf80029cc8
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-05-05 03:01:54 +0700

    feat(main.js): add task_4

--- homework_7/main.js
+++ homework_7/main.js
@@ -29,3 +29,25 @@ f(); */
 // alert( bound.test );
 
 /* После применения  bind у оригинальной функции sayHi так и останется свойство test, а после bind создается функция обертка bound, внутри которой движок хранит ссылку на оригинал и this, но эти данные недоступны, поэтому bound.test === undefined */
+
+/* 4. Вызов askPassword() в приведённом ниже коде должен проверить пароль и затем вызвать user.loginOk/loginFail в зависимости от ответа.
+Однако, его вызов приводит к ошибке. Почему? */
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", "");
+//   if (password == "rockstar") ok();
+//   else fail();
+// }
+// let user = {
+//   name: "Вася",
+//   loginOk() {
+//     alert(`${this.name} logged in`);
+//   },
+//   loginFail() {
+//     alert(`${this.name} failed to log in`);
+//   },
+// };
+// askPassword(user.loginOk, user.loginFail);
+
+/* askPassword работает неправильно, потому что мы передаём методы объекта как обычные функции, из-за чего при их вызове теряется контекст this. Чтобы исправить это, нужно использовать bind(user), который создаёт новую функцию с зафиксированным this:
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+*/
