338e96668932a9e35aad977e8e45bfa5804b4d43
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-05-05 03:19:16 +0700

    feat(main.js): add task_5

--- homework_7/main.js
+++ homework_7/main.js
@@ -51,3 +51,20 @@ f(); */
 /* askPassword работает неправильно, потому что мы передаём методы объекта как обычные функции, из-за чего при их вызове теряется контекст this. Чтобы исправить это, нужно использовать bind(user), который создаёт новую функцию с зафиксированным this:
 askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
 */
+
+/* 5. Объект user был изменён. Теперь вместо двух функций loginOk/loginFail у него есть только одна – user.login(true/false).
+Что нужно передать в вызов функции askPassword в коде ниже, чтобы она могла вызывать функцию user.login(true) как ok и функцию user.login(false) как fail? */
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+let user = {
+  name: "John",
+  login(result) {
+    alert(this.name + (result ? " logged in" : " failed to log in"));
+  },
+};
+
+/* Ответ */
+askPassword(user.login.bind(user, true), user.login.bind(user, false));
