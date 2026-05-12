c8e8f5092c14fc122cd3f0ae259e8634ad2cd180
Dmitry Fyodorov <fyodorov.tech@gmail.com>
2026-05-12 09:14:14 +0700

    feat(main.js): add task_3

--- homework_8/main.js
+++ homework_8/main.js
@@ -22,16 +22,113 @@
 // console.log(worker.getFullName());
 
 /* 2. Напишите новый класс Boss, этот класс наследуется от класса Worker и прошлого задания. Появляется новые свойство: workers - количество работников. И зарплата считается по другому: произведение (умножение) ставки rate на количество отработанных дней и на количество работников. */
+// class Worker {
+//   constructor(name, surname, rate, days) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.rate = rate;
+//     this.days = days;
+//   }
+
+//   getSalary() {
+//     return this.rate * this.days;
+//   }
+
+//   getFullName() {
+//     return `${this.name} ${this.surname}`;
+//   }
+// }
+
+// class Boss extends Worker {
+//   constructor(name, surname, rate, days, workers) {
+//     super(name, surname, rate, days);
+//     this.workers = workers;
+//   }
+
+//   getSalary() {
+//     return this.rate * this.days * this.workers;
+//   }
+// }
+
+// const boss = new Boss("Иван", "Шлыков", 8000, 25, 4);
+// console.log(boss);
+// console.log(boss.getFullName());
+// console.log(boss.getSalary());
+
+/* 3. Модифицируйте класс Worker из предыдущей задачи следующим образом: для свойства rate и для свойства days сделайте и методы-сеттеры и методы-геттеры для их чтения. */
+
+/* Вариант 1. Вообще не вижу смысла такого подхода в создании геттеров и сеттеров, ведь всё равно имеем доступ к свойствам на прямую, поэтому сделаю ещё другой вариант*/
+// class Worker {
+//   constructor(name, surname, rate, days) {
+//     this.name = name;
+//     this.surname = surname;
+//     this._rate = rate;
+//     this._days = days;
+//   }
+
+//   get rate() {
+//     return this._rate;
+//   }
+
+//   set rate(value) {
+//     this._rate = value;
+//   }
+
+//   get days() {
+//     return this._days;
+//   }
+
+//   set days(value) {
+//     this._days = value;
+//   }
+
+//   getSalary() {
+//     return this._rate * this._days;
+//   }
+
+//   getFullName() {
+//     return `${this.name} ${this.surname}`;
+//   }
+// }
+
+// const worker = new Worker("Дмитрий", "Фёдоров", 5000, 22);
+// worker.rate = 6000;
+// worker.days = 23;
+// console.log(worker.getFullName());
+// console.log(worker.rate);
+// console.log(worker.days);
+// console.log(worker.getSalary());
+
+/* Варинт 2 */
 class Worker {
+  #rate;
+  #days;
+
   constructor(name, surname, rate, days) {
     this.name = name;
     this.surname = surname;
-    this.rate = rate;
-    this.days = days;
+    this.#rate = rate;
+    this.#days = days;
+  }
+
+  get rate() {
+    return this.#rate;
+  }
+
+  set rate(value) {
+    this.#rate = value;
+  }
+
+  get days() {
+    return this.#days;
+  }
+
+  set days(value) {
+    this.#days = value;
   }
 
   getSalary() {
-    return this.rate * this.days;
+    return this.#rate * this.#days;
   }
 
   getFullName() {
@@ -39,18 +136,11 @@ class Worker {
   }
 }
 
-class Boss extends Worker {
-  constructor(name, surname, rate, days, workers) {
-    super(name, surname, rate, days);
-    this.workers = workers;
-  }
-
-  getSalary() {
-    return this.rate * this.days * this.workers;
-  }
-}
-
-const boss = new Boss("Иван", "Шлыков", 8000, 25, 4);
-console.log(boss);
-console.log(boss.getFullName());
-console.log(boss.getSalary());
+const worker = new Worker("Дмитрий", "Фёдоров", 5000, 22);
+worker.rate = 6000;
+worker.days = 23;
+console.log(worker.getFullName());
+console.log(worker.rate);
+console.log(worker.days);
+console.log(worker.getSalary());
+console.log(worker);
